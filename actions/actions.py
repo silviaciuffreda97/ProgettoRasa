7886e4804092f1cf417072eaeb13ca4b3c3d1e75
Silvia Ciuffreda <silviaciuffreda@MacBook-Pro-di-Silvia.local>
2023-05-17 20:23:44 +0200

    Terminata funzionalità chiedere informazioni a partire dal titolo del libro. In seguito raffinare per tenere conto del caso in cui il titolo del libro ha più corrispondenze (tipo signore degli anelli 1,2). Terminare le trame dei libri nel csv

--- actions/actions.py
+++ actions/actions.py
@@ -28,23 +28,125 @@ class ActionInfoLibro(Action):
     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
         titolo_libro = str(tracker.get_slot('nome_libro')).lower()  # Nome dello slot da prendere: nome_libro
-  
+
         libro = pd.read_csv('datasets/libri.csv', encoding="UTF-8", sep=";")
 
-        try:
-            autore = libro[libro['Titolo'] == titolo_libro]['Autore'].values[0]
-            genere = libro[libro['Titolo'] == titolo_libro]['Genere'].values[0]
-            eta = libro[libro['Titolo'] == titolo_libro]['Età'].values[0]
-           # prezzo = libro[libro['Titolo'] == titolo_libro]['Prezzo'].values[0]
-            descrizione = libro[libro['Titolo'] == titolo_libro]['Descrizione'].values[0]
+        for index, row in libro.iterrows():
+            if row['Titolo'] == titolo_libro:
+                autore = row['Autore']
+                genere = row['Genere']
+                età = row['Età']
+                trama = row['Descrizione']
 
-            message = f"Il libro '{titolo_libro}' è stato scritto da {autore} ed appartiene al genere '{genere}'.\n'{titolo_libro}' è {descrizione}"
-        except:
-            message = f"Mi dispiace, non sono riuscito a trovare informazioni sull'autore del libro '{titolo_libro}'."
+                message = f"Il libro '{titolo_libro}' è stato scritto da {autore} e appartiene al genere '{genere}'.\n'{titolo_libro}' è {trama}"
+            else:
+                message = f"Mi dispiace, non sono riuscito a trovare informazioni sul libro '{titolo_libro}'. Prova a scrivere tutto in minuscolo o forse non è presente presso la nostra libreria."
+            
+            dispatcher.utter_message(text=message)
+
+            return [FollowupAction("utter_another_question")]
+        
+
+
+class ActionInfoPrezzo(Action):
+    def name(self) -> Text:
+        return "action_info_prezzo"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        # Estrarre l'entità dal messaggio precedente
+        
+        nome_libro = tracker.get_slot("nome_libro")
+
+        libro = pd.read_csv('datasets/libri.csv', encoding="UTF-8", sep=";")
+        # Fare qualcosa con l'entità
+        if nome_libro:
+            for index, row in libro.iterrows():
+             prezzo = row['Prezzo']
+          
+            # Utilizzare l'entità nel messaggio di risposta
+            message = f"Presso la nostra libreria il libro '{nome_libro}' è disponibile al prezzo di € {prezzo}."
+        else:
+            message = f"Nessun prezzo trovato."
 
         dispatcher.utter_message(text=message)
-
-        return []
-
-
+        return [SlotSet("nome_libro", nome_libro)]
     
+
+class ActionInfoEtàConsigliata(Action):
+    def name(self) -> Text:
+        return "action_info_età_consigliata"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        # Estrarre l'entità dal messaggio precedente
+        
+        nome_libro = tracker.get_slot("nome_libro")
+
+        libro = pd.read_csv('datasets/libri.csv', encoding="UTF-8", sep=";")
+        # Fare qualcosa con l'entità
+        if nome_libro:
+            for index, row in libro.iterrows():
+             età = row['Età']
+          
+            # Utilizzare l'entità nel messaggio di risposta
+            message = f"La lettura del libro '{nome_libro}' è consigliata per età a partire da {età}."
+        else:
+            message = f"Nessuna fascia d'età trovata."
+
+        dispatcher.utter_message(text=message)
+        return [SlotSet("nome_libro", nome_libro)]
+
+
+class ActionInfoEtàConsigliata(Action):
+    def name(self) -> Text:
+        return "action_info_reparto"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        # Estrarre l'entità dal messaggio precedente
+        
+        nome_libro = tracker.get_slot("nome_libro")
+
+        libro = pd.read_csv('datasets/libri.csv', encoding="UTF-8", sep=";")
+        # Fare qualcosa con l'entità
+        if nome_libro:
+            for index, row in libro.iterrows():
+             reparto = row['Reparto']
+             scaffale = row['Scaffale']
+          
+            # Utilizzare l'entità nel messaggio di risposta
+            message = f"Il libro '{nome_libro}' è collocato nella seguente posizione:\n Reparto:{reparto} Scaffale:{scaffale}.\n\n"
+        else:
+            message = f"Nessuna collocazione trovata."
+
+        dispatcher.utter_message(text=message)
+        return [FollowupAction("utter_more_info"), AllSlotsReset()]
+
+
+
+
+
+
+# class ActionInfoLibro(Action):
+
+#     def name(self) -> Text:
+#         return "action_info_libro"
+    
+#     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+#         titolo_libro = str(tracker.get_slot('nome_libro')).lower()  # Nome dello slot da prendere: nome_libro
+  
+#         libro = pd.read_csv('datasets/libri.csv', encoding="UTF-8", sep=";")
+
+#         try:
+#             autore = libro[libro['Titolo'] == titolo_libro]['Autore'].values[0]
+#             genere = libro[libro['Titolo'] == titolo_libro]['Genere'].values[0]
+#             eta = libro[libro['Titolo'] == titolo_libro]['Età'].values[0]
+#            # prezzo = libro[libro['Titolo'] == titolo_libro]['Prezzo'].values[0]
+#             descrizione = libro[libro['Titolo'] == titolo_libro]['Descrizione'].values[0]
+
+#             message = f"Il libro '{titolo_libro}' è stato scritto da {autore} ed appartiene al genere '{genere}'.\n'{titolo_libro}' è {descrizione}"
+#         except:
+#             message = f"Mi dispiace, non sono riuscito a trovare informazioni sull'autore del libro '{titolo_libro}'."
+
+#         dispatcher.utter_message(text=message)
+
+#         return []
